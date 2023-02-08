03588272bc87f5deb457ae4c4fe0196cfcfa6947
Holden D Reed <holdendreed@hotmail.com>
2023-02-07 19:07:43 -0600

    Made main interface

---
+++ Zion-QuickTrippin/MainInterface.cs
@@ -0,0 +1,50 @@
+﻿namespace Zion_QuickTrippin;
+
+public class MainInterface
+{
+
+    public bool Running = true;
+    public void Show()
+    { 
+    while (Running)
+	{
+            Console.WriteLine("Management Interface");
+            Console.WriteLine(@"
+1. Add New District
+2. Add New Store
+3. Add New Employee
+4. Input Store Sales
+5. Generate District Report
+6. Exit");
+            string inputText = Console.ReadLine();
+            int numericInput = 0;
+            bool IsNumber = int.TryParse(inputText, out numericInput);
+            while (IsNumber != true || numericInput <= 0 || numericInput >= 7)
+            {
+                Console.WriteLine("Please enter a valid number.");
+                inputText = Console.ReadLine();
+                IsNumber = int.TryParse(inputText, out numericInput);
+            }
+
+            if (numericInput == 1)
+            {
+                Console.WriteLine("Adding district");
+            } else if (numericInput == 2) 
+            {
+                Console.WriteLine("Adding district");
+            } else if (numericInput == 3) 
+            {
+                Console.WriteLine("Adding district");
+            } else if (numericInput == 4) 
+            {
+                Console.WriteLine("Adding district");
+            } else if (numericInput == 5)
+            {
+                Console.WriteLine("Adding district");
+            } else if (numericInput == 6) 
+            {
+               Running= false;
+            }
+	}
+}
+}
